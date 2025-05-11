a838a371436ccca0976d02a5b5b8e55eadddf4e6
BimsaraSW <bimsarasw@gmail.com>
2025-05-11 13:45:53 +0530

    cors config corected

--- backend/src/main/java/backend/config/CorsConfig.java
+++ backend/src/main/java/backend/config/CorsConfig.java
@@ -21,17 +21,5 @@ public class CorsConfig {
             }
         };
     }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+
 }
